ca6d822864ce972eb7bafbc0893c47323381b708
dadiallo <d.abdoulayead28@gmail.com>
2023-01-07 16:50:12 +0000

    Odc Interview begins

---
+++ src/test/java/com/odk/odcinterview/OdcInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odk.odcinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdcInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
